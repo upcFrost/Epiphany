12358a229c283c3a9c577a20d35ea7092ad41098
Petr Belyaev <upcfrost@yandex-team.ru>
2018-03-12 19:18:47 +0300

    Revised callee-saved regs spill-restore, removed a lot of black magic

--- EpiphanyFrameLowering.cpp
+++ EpiphanyFrameLowering.cpp
@@ -167,7 +167,7 @@ void EpiphanyFrameLowering::emitEpilogue(MachineFunction &MF,
   // if framepointer enabled, set it to point to the stack pointer.
   if (hasFP(MF)) {
     // Restore old LR and FP from SP + offset
-    BuildMI(MBB, MBBI, dl, TII.get(LDRi64), LR).addReg(SP).addImm(StackSize).setMIFlag(MachineInstr::FrameSetup);
+    BuildMI(MBB, MBBI, dl, TII.get(LDRi64), LR).addReg(SP).addImm(StackSize).setMIFlag(MachineInstr::FrameDestroy);
   }
 
   // Adjust stack.
@@ -229,15 +229,19 @@ bool EpiphanyFrameLowering::hasReservedCallFrame(const MachineFunction &MF) cons
     !MFI.hasVarSizedObjects();
 }
 
-// FIXME: this function is quite hackie, as it relies on knowledge of LLVM's frame alloc
-// mechanism
-bool EpiphanyFrameLowering::assignCalleeSavedSpillSlots(MachineFunction &MF, 
+/// Assign callee-saved regs to frame indexes
+///
+/// \param MF Machine function
+/// \param TRI Register info
+/// \param CSI Callee-save regs info
+/// \return True if success
+bool EpiphanyFrameLowering::assignCalleeSavedSpillSlots(MachineFunction &MF,
     const TargetRegisterInfo *TRI, std::vector<CalleeSavedInfo> &CSI) const {
+  // TODO: Probably this method can be moved completely into reimplemented determineCalleeSaves()
+  // as it not only assigns but also redefines paired regs
   if (CSI.empty())
     return true; // Early exit if no callee saved registers are modified!
 
-  unsigned MinCSFrameIndex = std::numeric_limits<unsigned>::max();
-  unsigned MaxCSFrameIndex = 0;
   MachineFrameInfo &MFI = MF.getFrameInfo();
 
   unsigned NumFixedSpillSlots;
@@ -245,145 +249,71 @@ bool EpiphanyFrameLowering::assignCalleeSavedSpillSlots(MachineFunction &MF,
 
   // Now that we know which registers need to be saved and restored, allocate
   // stack slots for them.
-  bool Pair = false;
-  int PrevFrameIdx = -1;
-  for (size_t i = 0; i < CSI.size(); i++) {
-    auto CS = CSI.at(i);
-    unsigned Reg = CS.getReg();
-    const TargetRegisterClass *RC = TRI->getMinimalPhysRegClass(Reg);
-
-    int FrameIdx;
-    if (TRI->hasReservedSpillSlot(MF, Reg, FrameIdx)) {
-      CS.setFrameIdx(FrameIdx);
+  for (auto CS = CSI.begin(); CS != CSI.end(); ++CS) {
+    unsigned Reg = CS->getReg();
+    if (Reg == Epiphany::LR) {
+      DEBUG(dbgs() << "Erasing LR from CSI, it will be handled by prologue/epilogue inserters\n");
+      CSI.erase(CS--);
       continue;
     }
 
-    // Check to see if this physreg must be spilled to a particular stack slot
-    // on this target.
+    int FrameIdx;
+    const TargetRegisterClass *RC = TRI->getMinimalPhysRegClass(Reg);
+    if (TRI->hasReservedSpillSlot(MF, Reg, FrameIdx)) {
+      CS->setFrameIdx(FrameIdx);
+      continue;
+    }
+
+    // Check to see if this physreg must be spilled to a particular stack slot on this target.
     const TargetFrameLowering::SpillSlot *FixedSlot = FixedSpillSlots;
-    while (FixedSlot != FixedSpillSlots + NumFixedSpillSlots && FixedSlot->Reg != Reg) {
+    const TargetFrameLowering::SpillSlot *LastFixedSlot = FixedSlot + NumFixedSpillSlots;
+    while (FixedSlot != LastFixedSlot && FixedSlot->Reg != Reg) {
       ++FixedSlot;
     }
 
-    if (FixedSlot == FixedSpillSlots + NumFixedSpillSlots) {
+    if (FixedSlot != LastFixedSlot) {
+      // Spill it to the stack where we must and bail out
+      FrameIdx = MFI.CreateFixedSpillStackObject(RC->getSize(), FixedSlot->Offset);
+      CS->setFrameIdx(FrameIdx);
+      continue;
+    } else {
       // Nope, just spill it anywhere convenient.
       unsigned Align = RC->getAlignment();
       unsigned StackAlign = getStackAlignment();
 
       // Check if this index can be paired
-      unsigned suba, subb, sra = 0, srb = 0;
-      if(!Pair && (i+1 < CSI.size())) {
-        suba = CSI[i].getReg();
-        subb = CSI[i+1].getReg();
-        // Getting target class and matching superreg
-        const TargetRegisterClass *TRC = TRI->getMinimalPhysRegClass(suba) == &Epiphany::GPR32RegClass || 
-          TRI->getMinimalPhysRegClass(suba) == &Epiphany::GPR16RegClass ? 
-          &Epiphany::GPR64RegClass : &Epiphany::FPR64RegClass;
-        sra = TRI->getMatchingSuperReg (suba, Epiphany::isub_lo, TRC);
-        srb = TRI->getMatchingSuperReg (subb, Epiphany::isub_hi, TRC);
-        if( (!sra || !srb) || sra != srb){
-          srb = TRI->getMatchingSuperReg (suba, Epiphany::isub_hi, TRC);
-          sra = TRI->getMatchingSuperReg (subb, Epiphany::isub_lo, TRC);
+      unsigned sra = 0, srb = 0;
+      auto Next = CS;
+      Next++;
+      if (Next != CSI.end()) {
+        unsigned CurrentReg = CS->getReg();
+        unsigned NextReg = Next->getReg();
+        // Getting target class
+        const TargetRegisterClass *TRC = TRI->getMinimalPhysRegClass(CurrentReg) == &Epiphany::GPR32RegClass ||
+                                         TRI->getMinimalPhysRegClass(CurrentReg) == &Epiphany::GPR16RegClass
+                                         ? &Epiphany::GPR64RegClass
+                                         : &Epiphany::FPR64RegClass;
+        // Check if we can find superreg for paired regs
+        sra = TRI->getMatchingSuperReg(CurrentReg, Epiphany::isub_lo, TRC);
+        srb = TRI->getMatchingSuperReg(NextReg, Epiphany::isub_hi, TRC);
+        if ((!sra || !srb) || sra != srb) {
+          srb = TRI->getMatchingSuperReg(CurrentReg, Epiphany::isub_hi, TRC);
+          sra = TRI->getMatchingSuperReg(NextReg, Epiphany::isub_lo, TRC);
+        }
+
+        // Check if pair was formed
+        if ((sra && srb) && sra == srb) {
+          // Remove subregs and set superreg as Callee-saved
+          CSI.erase(CS--, ++Next);
+          CSI.emplace_back(sra);
+          continue;
         }
       }
-      if (!Pair && (sra && srb) && sra == srb) {
-        Pair = true;
-      }
 
-      // We may not be able to satisfy the desired alignment specification of
-      // the TargetRegisterClass if the stack alignment is smaller. Use the
-      // min.
+      // If unable to pair for some reason - just assign to the next frame index
       Align = std::min(Align, StackAlign);
       FrameIdx = MFI.CreateStackObject(RC->getSize(), Align, true);
-      if ((unsigned)FrameIdx < MinCSFrameIndex) MinCSFrameIndex = FrameIdx;
-      if ((unsigned)FrameIdx > MaxCSFrameIndex) MaxCSFrameIndex = FrameIdx;
-      // Swap indexes for paires
-      if (Pair && PrevFrameIdx != -1) {
-        // Create two local frame objects aligned to dword
-        bool StackGrowsDown = getStackGrowthDirection() == TargetFrameLowering::StackGrowsDown;
-        int64_t LocalFrameSize = (MFI.getLocalFrameSize() + 0x7) & ~0x7;
-        LocalFrameSize = StackGrowsDown ? -LocalFrameSize : LocalFrameSize;
-        LocalFrameSize += StackGrowsDown ? -4 : 4;
-        MFI.mapLocalFrameObject(PrevFrameIdx, LocalFrameSize);
-        LocalFrameSize += StackGrowsDown ? -4 : 4;
-        MFI.mapLocalFrameObject(FrameIdx, LocalFrameSize);
-        MFI.setLocalFrameSize(std::abs(LocalFrameSize));
-        MFI.setObjectAlignment(FrameIdx, 8);
-        CSI[i-1].setFrameIdx(FrameIdx);
-        CSI[i].setFrameIdx(PrevFrameIdx);
-        Pair = false;
-        PrevFrameIdx = -1;
-      } else if (Pair) {
-        PrevFrameIdx = FrameIdx;
-      } else {
-        CSI[i].setFrameIdx(FrameIdx);
-      }
-    } else {
-      // Spill it to the stack where we must.
-      FrameIdx = MFI.CreateFixedSpillStackObject(RC->getSize(), FixedSlot->Offset);
-      CS.setFrameIdx(FrameIdx);
-    }
-  }
-
-  return true;
-}
-
-// Spill callee-saved regs to stack
-bool EpiphanyFrameLowering::spillCalleeSavedRegisters(MachineBasicBlock &MBB,
-    MachineBasicBlock::iterator MI, const std::vector<CalleeSavedInfo> &CSI, const TargetRegisterInfo *TRI) const {
-  MachineFunction *MF = MBB.getParent();
-  const TargetInstrInfo &TII = *MF->getSubtarget().getInstrInfo();
-
-  // Debug output
-  DebugLoc DL;
-  if (MI != MBB.end()) {
-    DL = MI->getDebugLoc();
-  }
-
-  DEBUG(dbgs() << "\nCallee-saved regs in the current block:\n";
-      for (auto I = CSI.begin(), E = CSI.end(); I != E; ++I) {
-      TRI->dumpReg(I->getReg());
-      });
-
-  for (size_t i = 0; i < CSI.size(); i++) {
-    // Add the callee-saved register as live-in.
-    // It's killed at the spill, unless the register is LR and return address
-    // is taken.
-    unsigned Reg = CSI[i].getReg();
-    bool IsRAAndRetAddrIsTaken = (Reg == Epiphany::LR) && MF->getFrameInfo().isReturnAddressTaken();
-    if (!IsRAAndRetAddrIsTaken) {
-      MBB.addLiveIn(Reg);
-    }
-    bool IsKill = !IsRAAndRetAddrIsTaken;
-
-    // Try to pair the spill
-    bool Pair = false;
-    unsigned suba, subb, frameidx, sra = 0, srb = 0;
-    if(i+1 < CSI.size()){
-      suba = CSI[i].getReg();
-      subb = CSI[i+1].getReg();
-      // Getting target class and matching superreg
-      const TargetRegisterClass *RC = TRI->getMinimalPhysRegClass(suba) == &Epiphany::GPR32RegClass ? &Epiphany::GPR64RegClass : &Epiphany::FPR64RegClass;
-      sra = TRI->getMatchingSuperReg (suba, Epiphany::isub_lo, RC);
-      srb = TRI->getMatchingSuperReg (subb, Epiphany::isub_hi, RC);
-      frameidx = CSI[i].getFrameIdx();
-      if( (!sra || !srb) || sra != srb){
-        srb = TRI->getMatchingSuperReg (suba, Epiphany::isub_hi, RC);
-        sra = TRI->getMatchingSuperReg (subb, Epiphany::isub_lo, RC);
-        frameidx = CSI[i+1].getFrameIdx();
-      }
-    }
-    if ((sra && srb) && sra == srb) {
-      Pair = true;
-      const TargetRegisterClass *RC = TRI->getMinimalPhysRegClass(sra);
-      TII.storeRegToStackSlot(MBB, MI, sra, IsKill, frameidx, RC, TRI);
-      i++;
-    }
-
-    // Insert the spill to the stack frame.
-    if (!Pair) {
-      const TargetRegisterClass *RC = TRI->getMinimalPhysRegClass(Reg);
-      TII.storeRegToStackSlot(MBB, MI, Reg, IsKill, CSI[i].getFrameIdx(), RC, TRI);
+      CS->setFrameIdx(FrameIdx);
     }
   }
 
@@ -410,14 +340,14 @@ bool EpiphanyFrameLowering::hasFP(const MachineFunction &MF) const {
       dbgs() << "\nHas FP: Frame address taken\n";
       };);
 
-  return (MF.getTarget().Options.DisableFramePointerElim(MF) || 
+  return (MF.getTarget().Options.DisableFramePointerElim(MF) ||
       TRI->needsStackRealignment(MF) ||
       MFI.hasVarSizedObjects() ||
       MFI.isFrameAddressTaken());
 }
 
 /// Set local frame max alignment to 8, used by EpiphanyLoadStoreOptimizer
-void EpiphanyFrameLowering::processFunctionBeforeFrameFinalized(MachineFunction &MF, 
+void EpiphanyFrameLowering::processFunctionBeforeFrameFinalized(MachineFunction &MF,
     RegScavenger *RS) const {
   MachineFrameInfo &MFI = MF.getFrameInfo();
   MFI.setLocalFrameMaxAlign(8);
